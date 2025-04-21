0410a3a2ec1a83e3a4e5e0d79a25cec1419ca377
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-04-21 17:28:44 +0800

    :tada: use flyway for DDLs :tada:

--- sampleapp/src/app.py
+++ sampleapp/src/app.py
@@ -5,11 +5,11 @@ from src.models.models import User, Order
 app = Flask(__name__)
 
 @app.route("/")
+
+
 @app.route("/health")
 def health():
     return {"status": "ok"}
 
 if __name__ == "__main__":
-    with app.app_context():
-        Base.metadata.create_all(bind=engine)
     app.run(host="0.0.0.0", port=5005)
